1379ae8e5439397879e0eb524fca3eb5434bb9fb
Christian <snamun1609@gmail.com>
2021-01-21 10:10:10 +0700

    add file

---
+++ src/pages/home/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "../home/style.css";
+import HomeLeft from "../../components/home_left";
+import HomeRight from "../../components/home_right";
+import Footer from "../../components/footer";
+import { Container, Row, Col } from "react-bootstrap";
+import Animate from "react-smooth";
+import Particles from "react-particles-js";
+import particles from "../../const/particle.js";
+import ReactGa from "react-ga";
+import { Helmet } from "react-helmet";
+
+function Analytics() {
+  ReactGa.initialize("UA-154721739-1");
+  ReactGa.pageview("motasimfoad.com - Home Screen");
+}
+
+function Home() {
+  Analytics();
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Portfolio | Nam Dong</title>
+        <link rel="canonical" href="https://namdtfu.github.io/portfolio/" />
+        <meta
+          name="description"
+          content="Nam Dong | Home - Minimalism | Frontend Developer | Software Engineer"
+        />
+      </Helmet>
+      <Animate to="1" from="0" attributeName="opacity">
+        <Particles params={particles} className="particle" />
+        <Container className="App-header" fluid={true}>
+          <Row className="App-main">
+            <Col xl={7} className="App-left">
+              <HomeLeft />
+            </Col>
+            <Col xl={5} className="App-right">
+              <HomeRight />
+            </Col>
+          </Row>
+          <Row className="App-footer">
+            <Footer />
+          </Row>
+        </Container>
+      </Animate>
+    </div>
+  );
+}
+
+export default Home;
