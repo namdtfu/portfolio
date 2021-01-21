afd0871f7e4c57b72f3165ed4c41a2d7a9dc7e55
Christian <snamun1609@gmail.com>
2021-01-21 10:09:37 +0700

    Updates

---
+++ precache-manifest.2a4a8ed87c92d0a9e648f1c8ffa3a937.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde997c22eb753c2858b2a04294883cc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9f0d5ebb32a16f138731",
+    "url": "/portfolio/static/css/2.efd9d441.chunk.css"
+  },
+  {
+    "revision": "aed58aa977a14a551cf9",
+    "url": "/portfolio/static/css/main.764f2f9c.chunk.css"
+  },
+  {
+    "revision": "9f0d5ebb32a16f138731",
+    "url": "/portfolio/static/js/2.2bf220fe.chunk.js"
+  },
+  {
+    "revision": "aed58aa977a14a551cf9",
+    "url": "/portfolio/static/js/main.98bf9ebb.chunk.js"
+  },
+  {
+    "revision": "1de174ee8703aa7f06d8",
+    "url": "/portfolio/static/js/runtime-main.4fce312f.js"
+  },
+  {
+    "revision": "5c653daace009c6d0d9c4fd8f7f265ff",
+    "url": "/portfolio/static/media/cv.5c653daa.png"
+  },
+  {
+    "revision": "54c8b12a4400ad41b9c408c170cc2070",
+    "url": "/portfolio/static/media/cv1.54c8b12a.png"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/portfolio/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/portfolio/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/portfolio/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "a7dc64e154b0e731e638fdc4fbc43263",
+    "url": "/portfolio/static/media/foad35.a7dc64e1.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/portfolio/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/portfolio/static/media/git.c31b5069.svg"
+  }
+]);
